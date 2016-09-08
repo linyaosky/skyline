42c6cd5775f3feafd8eed933826a15e3a9611c12
linyaosky <1245807623@qq.com>
2016-09-08 20:53:43 +0800

    初始化

---
+++ src/main/java/com/skyline/SkylineApplication.java
@@ -0,0 +1,12 @@
+package com.skyline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkylineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkylineApplication.class, args);
+	}
+}
